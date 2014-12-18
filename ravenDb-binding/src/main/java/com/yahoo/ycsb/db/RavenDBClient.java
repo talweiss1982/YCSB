43dd3debcc3d62d78c1d616cdc1f764761ad5ec3
Tal Weiss <tal@ayende.com>
2014-12-18 13:58:28 +0200

    some optimization of raven ycsb client

--- ravenDb-binding/src/main/java/com/yahoo/ycsb/db/RavenDBClient.java
+++ ravenDb-binding/src/main/java/com/yahoo/ycsb/db/RavenDBClient.java
@@ -3,54 +3,42 @@
  */
 package com.yahoo.ycsb.db;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
-import java.util.Vector;
-import java.util.concurrent.atomic.AtomicInteger;
-import com.yahoo.ycsb.ByteArrayByteIterator;
-import com.yahoo.ycsb.ByteIterator;
-import com.yahoo.ycsb.DB;
-import com.yahoo.ycsb.DBException;
+import com.yahoo.ycsb.*;
 import net.ravendb.abstractions.basic.CloseableIterator;
 import net.ravendb.abstractions.data.JsonDocument;
 import net.ravendb.abstractions.json.linq.RavenJObject;
 import net.ravendb.client.IDocumentSession;
 import net.ravendb.client.IDocumentStore;
 import net.ravendb.client.connection.IDatabaseCommands;
-import net.ravendb.client.converters.ITypeConverter;
 import net.ravendb.client.document.BulkInsertOperation;
 import net.ravendb.client.document.DocumentStore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
 
 public class RavenDBClient extends DB {
     @Override
     public void init() throws DBException {
-        getStoreInstance().getConventions().getIdentityProperty();
     }
     @Override
     public int read(String table, String key, Set<String> fields, HashMap<String, ByteIterator> result) {
-        //System.out.println("Read, Table:" + table + " " +"Key:" + key);
-        IDocumentSession session = openSession();
-        RavenJObject obj = session.load(RavenJObject.class, key);
+        JsonDocument obj = getStoreInstance().getDatabaseCommands().get(key);
         for (String field : fields) {
-            result.put(field, new ByteArrayByteIterator(obj.get(field).toString().getBytes()));
+            result.put(field, new StringByteIterator(obj.getDataAsJson().get(field).toString()));
         }
-        session.saveChanges();
-        closeSession(session);
         return 0;
     }
 
     @Override
     public int scan(String table, String startkey, int recordcount, Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
-        //System.out.println("Scan, Table:" + table + " " +"StartKey:" + startkey);
-        CloseableIterator<RavenJObject> iterator = getStoreInstance().getDatabaseCommands().streamDocs(null,null,null,Integer.parseInt(startkey.substring("user".length())),recordcount);
+        CloseableIterator<RavenJObject> iterator = getStoreInstance().getDatabaseCommands().streamDocs(null, startkey, null, 0, recordcount);
         while (iterator.hasNext()) {
-            HashMap<String, ByteIterator> docFields = new HashMap<String, ByteIterator>();
+            HashMap<String, ByteIterator> docFields = new HashMap<>();
             RavenJObject document = iterator.next();
             for (String field : fields) {
-                docFields.put(field, new ByteArrayByteIterator(document.get(field).toString().getBytes()));
+                docFields.put(field, new StringByteIterator(document.get(field).toString()));
             }
             result.add(docFields);
         }
@@ -60,33 +48,33 @@ public class RavenDBClient extends DB {
 
     @Override
     public int update(String table, String key, HashMap<String, ByteIterator> values) {
-        //System.out.println("update, Table:" + table + " " +"Key:" + key);
-        IDocumentSession session = openSession();
-        RavenJObject document = session.load(RavenJObject.class,key);
+        IDatabaseCommands databaseCommands = getStoreInstance().getDatabaseCommands();
+        JsonDocument obj = databaseCommands.get(key);
+        RavenJObject document = obj.getDataAsJson();
         for (Map.Entry<String,ByteIterator> entry : values.entrySet())
         {
-            document.add(entry.getKey(),entry.getValue().toArray());
+            document.add(entry.getKey(),entry.getValue().toString());
         }
-        session.saveChanges();
-        closeSession(session);
+        databaseCommands.put(key, null, document, obj.getMetadata());
         return 0;
     }
 
+
     @Override
     public int insert(String table, String key, HashMap<String, ByteIterator> values) {
-        //System.out.println("insert, Table:" + table + " " +"Key:" + key);
-        //IDocumentSession session = openSession();
+        RavenJObject insertMetadata = new RavenJObject();
+        insertMetadata.add("Raven-Entity-Name", "Users");
+
         RavenJObject obj = new RavenJObject();
         for (Map.Entry<String,ByteIterator> entry : values.entrySet())
         {
-            obj.add(entry.getKey(),entry.getValue().toArray());
+            obj.add(entry.getKey(),entry.getValue().toString());
         }
-        //session.store(obj);
-        //session.saveChanges();
-        //closeSession(session);
         try {
-            getBulkOperation().store(obj);
-        } catch (Exception e) {}
+            getBulkOperation().store(obj, insertMetadata, key);
+        } catch (Exception e) {
+            return -1;
+        }
         return 0;
     }
 
@@ -99,7 +87,9 @@ public class RavenDBClient extends DB {
     public void cleanup() throws DBException {
         try {
             getBulkOperation().close();
-        } catch (Exception e){}
+        } catch (Exception e){
+
+        }
     }
     public static void main(String[] args)
     {
@@ -115,41 +105,22 @@ public class RavenDBClient extends DB {
     }
 
     public static IDocumentStore getStoreInstance() {
-        if (store == null) {
-            synchronized (RavenDBClient.class) {
-                if (store == null) {
-                    store = createStore();
-                }
-            }
-        }
         return store;
     }
-    private  static void closeSession(IDocumentSession session)
-    {
-        try
-        {
-            session.close();
-        } catch (Exception e) {
 
-        }
-    }
     private static IDocumentStore createStore() {
         IDocumentStore store = new DocumentStore("http://localhost:8080", "Test");
         store.initialize();
         return store;
     }
 
-    public static IDocumentSession openSession()
-    {
-        return getStoreInstance().openSession();
-    }
-
     public BulkInsertOperation getBulkOperation()
     {
-        if (bulkOperation == null) bulkOperation = store.bulkInsert();
+        if (bulkOperation == null)
+            bulkOperation = store.bulkInsert();
         return bulkOperation;
     }
 
     private BulkInsertOperation bulkOperation;
-    private static IDocumentStore store;
+    private static IDocumentStore store = createStore();
 }
